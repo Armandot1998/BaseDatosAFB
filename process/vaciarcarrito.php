7f087f3b0dd84f807e42ff1598074f8e199e22ca
Armandot1998 <33724801+Armandot1998@users.noreply.github.com>
2019-01-16 19:14:08 -0500

    Hola

--- process/vaciarcarrito.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['producto']);
-unset($_SESSION['contador']);
-unset($_SESSION['sumaTotal']);
-?>
-<script>
-    window.location = "../index.php";
-</script>
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCbNCgP4YhMDdpBfsR6y0V3S1NKV6ONo7o",
+    authDomain: "angular-7cc5f.firebaseapp.com",
+    databaseURL: "https://angular-7cc5f.firebaseio.com",
+    projectId: "angular-7cc5f",
+    storageBucket: "angular-7cc5f.appspot.com",
+    messagingSenderId: "772522711282"
+  }
+};
